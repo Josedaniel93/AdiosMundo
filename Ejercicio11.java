f3ac8904c9004335b14de4e4f1ae2dd6c4f6e661
josedaniel93 <josedanielmartilabrador@gmail.com>
2019-10-03 18:19:38 +0200

    Colores definidos

--- Ejercicio11.java
+++ Ejercicio11.java
@@ -4,10 +4,12 @@ public class Ejercicio11 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String naranja = "\033[33m";
+		String azul = "\033[34m";
+
+		System.out.println(naranja+"Adios Mundo");
 		
-		System.out.println("Adios Mundo");
-		
-		System.out.println("Pero volvere");		
+		System.out.println(azul+"Pero volvere");		
 	
 		
 	}
